342a68232f390f5215ea3caadd08beb2d5387061
bRad-95 <bryantramirez@ac.c4q.nyc>
2017-12-20 19:25:44 -0500

    complete part01

---
+++ app/src/main/java/nyc/c4q/unit04mid_unitassessment/MainFragment.java
@@ -0,0 +1,50 @@
+package nyc.c4q.unit04mid_unitassessment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+  private View rootview;
+
+  public MainFragment() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+
+    rootview = inflater.inflate(R.layout.fragment_main, container, false);
+
+    Button button = (Button) rootview.findViewById(R.id.display_fragment_button);
+
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        DisplayFragment displayFragment = new DisplayFragment();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, displayFragment);
+        fragmentTransaction.addToBackStack("next");
+        fragmentTransaction.commit();
+      }
+    });
+    // Inflate the layout for this fragment
+    return rootview;
+  }
+
+}
